b7518b8cb476ff2dc74c84ff4fa0f959a2b202db
Solomon Song <solomon@Solomons-MacBook-Pro.local>
2019-10-23 16:14:49 -0600

    inital commit

---
+++ register_birth.py
@@ -0,0 +1,10 @@
+# Requirements:
+# births(regno, fname, lname, regdate, regplace, gender, f_fname, f_lname, m_fname, m_lname)
+# regdate will be the current date when command is inputted
+# regno is a random number of 6 digits (UNIQUE)
+
+
+def register_birth():
+    
+
+    return
